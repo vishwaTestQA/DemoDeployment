711ae7c9f71f97765680890b51678ae5123fe204
vishwa <vishwa80123@gmail.com>
2025-06-16 08:15:44 +0530

    11th 15-06

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
     <main className='main'>
-       "No items to display"
+       {/* <Link to='/dataPage'></Link> */}
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusantium dignissimos earum obcaecati? Obcaecati id, nam ea possimus minima voluptas doloribus, minus placeat tempora optio ut laborum totam deserunt natus!
+
     </main>
   )
 }
